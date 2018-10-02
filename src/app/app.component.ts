c313cf734bd9957f6b97ff74c48895c544b08b9d
Malcom <mm@hhpty.com>
2018-10-02 17:25:46 +0200

    scheduler is undefined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
-import { Store, select } from '@ngrx/store';
+import { Component } from '@angular/core';  
+import { Store, select } from '@ngrx/store';  
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   template: `
-  Cart: {{cart.length}}
-  <router-outlet></router-outlet>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1 class="text-center">Online Store</h1>
+        <h6 class="text-center"><a [routerLink]="['/cart']">Cart: {{(cart | async).length}}</a></h6>
+        <hr />
+      </div>
+    </div>
+    <router-outlet></router-outlet>
+  </div>
   `,
   styles: []
 })
-export class AppComponent {
+
+export class AppComponent {  
   title = 'app';
-  cart: Array<any>
+  constructor(private store: Store<any>) {}
 
-  constructor( private _store: Store<any>){
+  cart: Observable<Array<any>>
 
-  }
-
-  ngOnInit(){
-    this._store.select('cart').subscribe( state => this.cart =state);
+  ngOnInit() {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    this.cart = this.store.select('cart')
   }
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from "./../../store/market"; 
 
 @Component({
   selector: 'app-products',
-  template: `
-    <p>
-      products works!
-    </p>
-  `,
-  styles: []
+  templateUrl:'products.component.html',
+  styleUrls: ['products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products = PRODUCTS;
 
   constructor() { }
 
